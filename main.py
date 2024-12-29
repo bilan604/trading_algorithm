0c9466cb5392e89eede122c94042e9df862a591f
Lan <lan@LNMacBook-Pro.local>
2024-12-28 19:35:07 -0500

    update files

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def run_backtest():
     REG = GradientBoostingRegressor(random_state=0)
     tb = TradingBot(df_btc_name, REG, CUTOFF_LOWER=1.2, CUTOFF_UPPER=100, \
                     SLPERC=0.05, TPPERC=0.05, \
-                    NP_CUTOFF_PCT=0.8, shorts=True, \
+                    NP_CUTOFF_PCT=0.8, shorts=False, \
                     window_sizes=[1, 3, 9, 15, 30, 60, 120, 240, 480, 960])
     df_btc_name, df_btc_backtest = tb.initialize_window_signaler_for_backtesting()
     results = backtest(tb, [df_btc_backtest], [df_btc_name], 'data_forex')
