c185369d415d61e5c558a5435a1f95981a2db285
Lan <lan@LNMacBook-Pro.local>
2025-01-14 17:57:43 -0500

    add new testing branch

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-
-from sklearn.ensemble import GradientBoostingRegressor
-from serve import perform_routine
-from trading_bot import TradingBot
-from trade_handler import TradeHandler
-
-from coinbase.rest import RESTClient
+from testing import *
 
 
 def get_env(path=".env"):
@@ -22,41 +16,15 @@ def get_env(path=".env"):
     return env
 
 
-
-if __name__ == "__main__":
-    csv_path = 'csvs/updating_btc.csv'
-    cached_prices_path = 'cached_prices.txt'
-    REG = GradientBoostingRegressor(random_state=0)
-    model = TradingBot(df_name=csv_path, \
-                       REG=REG, \
-                       CUTOFF_LOWER=0.7, CUTOFF_UPPER=100, \
-                       SLPERC=0.05, TPPERC=0.05, \
-                       NP_CUTOFF_PCT=0.90, \
-                       shorts=False, \
-                       window_sizes=[1, 3, 9, 15, 30, 60, 120, 240, 480, 960])
-    
-    api_key = get_env()["CDP_API_KEY"]
-    api_secret = get_env()["CDP_API_KEY_PRIVATE_KEY"]
-    client = RESTClient(api_key=api_key, api_secret=api_secret)
-    
-    trade_handler = TradeHandler(model=model, \
-                                    client=client, \
-                                    margin=1.0, \
-                                    trade_size=0.45)
-    
-    perform_routine(csv_path, cached_prices_path, trade_handler)
-
-
-
-#if __name__ == '__main__':
+if __name__ == '__main__':
     # using the backtest library, comparison of my trading algorithm v.s. michael harris trading method
-    #results = run_backtest()
+    results = run_backtest()
     # compares XGBot against highest returning bot of 1000 randomly trading bots
-    #multitest()
+    multitest()
     # simulates trading with Michael Harris indicator
-    #simulate_michael_harris()
+    simulate_michael_harris()
     # compares XGBot against randomly trading bot
-    #simulate_both()
+    simulate_both()
     # min, max, and average stats of 35 XGBots on different train/test split sizes
-    #view_spread()
+    view_spread()
     
