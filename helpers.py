cf60fdb1561299a58ffed938eab40d7a392a5820
Lan <lan@LNMacBook-Pro.local>
2025-01-15 12:49:43 -0500

    dependencies, VM streamling

--- helpers.py
+++ helpers.py
@@ -47,3 +47,25 @@ def get_btc_usd_price():
     time = obj['time']['updatedISO']
     price, time = format_price_time(btc_price, time)
     return price, time
+
+def get_IP():
+    import requests
+    from bs4 import BeautifulSoup
+
+    url = 'https://whatismyv6.com/'
+
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.content, 'html.parser')
+
+    tables = soup.find_all('table')
+    table = None
+    for tbl in list(tables):
+        if 'You are connecting with an' in tbl.text:
+            table = tbl
+            break
+
+    table_elements = table.text.strip().split("\n")    
+    print(table_elements[0])
+    IP = table_elements[1]
+    return IP
+
